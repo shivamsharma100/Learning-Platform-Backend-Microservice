e7373eb31b5a9487f0ad19b0701caecd0eb2e0f5
shivam.aj.sharma <shivam.aj.sharma@kamereon.io>
2026-01-07 18:10:51 +0530

    Adding test cases

--- src/test/java/com/example/course/myCourse/LessonControllerTest.java
+++ src/test/java/com/example/course/myCourse/LessonControllerTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class LessonControllerUnitTest {
+class LessonControllerTest {
 
     @Mock
     private LessonService lessonService;
